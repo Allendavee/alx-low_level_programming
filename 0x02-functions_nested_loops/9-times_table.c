31ba50a6a2d8779c0411eb3c6a12501730efa5ae
allendavee <ugiomohlilmars@gmail.com>
2023-02-21 16:58:02 +0000

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,16 @@ void times_table(void)
 
 			times = j * i;
 
-			_putchar((times % 10) + '0');
+			if (times <= 9)
+			{
+				_putchar(times + '0');
+			}
+			else if (times > 9)
+			{
+				_putchar((times / 10) + '0');
+				_putchar((times % 10) + '0');
+			}
+
 			if (j < 9)
 			{
 				_putchar(',');
