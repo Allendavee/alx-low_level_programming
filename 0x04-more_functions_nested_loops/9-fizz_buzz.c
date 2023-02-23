2c54f80a6c4538abf373546b10cc5531aa92717e
allendavee <ugiomohlilmars@gmail.com>
2023-02-23 15:02:56 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 
 	i = 1;
 
-	while (i <= 100)
+	while (i < 100)
 	{
 		if (i % 3 == 0)
 		{
@@ -31,7 +31,7 @@ int main(void)
 		}
 		i++;
 	}
-	printf("\n");
+	printf("Buzz\n");
 
 	return (0);
 }
