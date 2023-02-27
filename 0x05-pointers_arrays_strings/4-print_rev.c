db961e3eb8e716b15b771724745b5d1a40fb9688
allendavee <ugiomohlilmars@gmail.com>
2023-02-27 15:41:18 +0000

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,7 +7,7 @@
  */
 void print_rev(char *s)
 {
-	int i, lstr = 0;
+	int i, j, lstr = 0;
 	char temp;
 
 	while (s[lstr] != '\0')
@@ -20,8 +20,11 @@ void print_rev(char *s)
 		temp = s[i];
 		s[i] = s[lstr - i - 1];
 		s[lstr - i - 1] = temp;
+	}
 
-		_putchar(s);
+	for (j = 0; j < lstr; j++)
+	{
+		_putchar(s[j]);
 	}
 
 	_putchar('\n');
