ba3cc21ca077c3c74b05f84104b3789367199791
allendavee <ugiomohlilmars@gmail.com>
2023-02-27 17:45:17 +0000

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,10 +8,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	char ans;
+	int i;
 
-	*dest = *src;
-	ans = *dest;
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+		dest[i] = *(src + i);
+	}
+	dest[i] = '\0';
 
-	return (ans);
+	return (dest);
 }
