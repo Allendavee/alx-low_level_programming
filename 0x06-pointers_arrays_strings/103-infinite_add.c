5b7769900ffef4984a4720366aa8caedd1bd42f7
allendavee <ugiomohlilmars@gmail.com>
2023-03-04 23:33:12 +0000

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,32 +13,32 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, op, bg, dr1, dr2, add = 0;
+	int i = 0, j = 0, carry, max_length, dr1, dr2, add = 0;
 
 	while (*(n1 + i) != '\0')
 		i++;
 	while (*(n2 + j) != '\0')
 		j++;
 	if (i >= j)
-		bg = i;
+	       	max_length = i;
 	else
-		bg = j;
-	if (size_r <= bg + 1)
+		max_length = j;
+	if (size_r <= max_length + 1)
 		return (0);
-	r[bg + 1] = '\0';
+	r[max_length + 1] = '\0';
 	i--, j--, size_r--;
 	dr1 = *(n1 + i) - 48, dr2 = *(n2 + j) - 48;
-	while (bg >= 0)
+	while (max_length >= 0)
 	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
+		carry = dr1 + dr2 + add;
+		if (carry >= 10)
+			add = carry / 10;
 		else
 			add = 0;
-		if (op > 0)
-		*(r + bg) = (op % 10) + 48;
+		if (carry > 0)
+		*(r + max_length) = (carry % 10) + 48;
 		else
-			*(r + bg) = '0';
+			*(r + max_length) = '0';
 		if (i > 0)
 			i--, dr1 = *(n1 + i) - 48;
 		else
@@ -47,7 +47,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			j--, dr2 = *(n2 + j) - 48;
 		else
 			dr2 = 0;
-		bg--, size_r--;
+		max_length--, size_r--;
 	}
 	if (*(r) == '0')
 		return (r + 1);
