a311df668d0731d49cab24f1d684042be04dbdc4
allendavee <ugiomohlilmars@gmail.com>
2023-03-07 21:39:16 +0000

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Function that prints a string
+ * in rverse
+ * @s: Pointer to string
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_puchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
