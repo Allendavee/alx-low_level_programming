42892df78b9b93e6b0a29c509cd5184f53f553b9
allendavee <ugiomohlilmars@gmail.com>
2023-03-14 12:05:03 +0000

    main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,7 +8,11 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
+int strlen(char *s);
+void strcpy(char *dst, char *src);
+int strcmp(const char *s1, char *s2);
 char **strtow(char *str);
+
 
 
 #endif
