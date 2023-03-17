645e1c595b082bf96d7ac2ca921251f403f294e5
allendavee <ugiomohlilmars@gmail.com>
2023-03-17 08:28:45 +0000

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,34 +2,88 @@
 #include <stdlib.h>
 
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
+ * _isdigit - Checks if a character is a digit
+ *
+ * @c: Character to be checked
+ *
+ * Return: 1 if c is a digit, 0 otherwise
  */
-int main(int argc, char *argv[])
+int _isdigit(char c)
 {
-	unsigned long mul;
-	int i, j;
+	return (c >= '0' && c <= '9');
+}
 
+/**
+ * error_exit - Prints an error message and exits with status 98
+ */
+void error_exit(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - Entry point
+ *
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ *
+ * Return: 0 on success, 98 on failure
+ */
+int main(int argc, char **argv)
+{
 	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
-		}
+		error_exit();
 
+	char *num1 = argv[1];
+	char *num2 = argv[2];
+
+	for (int i = 0; num1[i]; i++)
+		if (!_isdigit(num1[i]))
+			error_exit();
+
+	for (int i = 0; num2[i]; i++)
+		if (!_isdigit(num2[i]))
+			error_exit();
+
+	int len1 = 0, len2 = 0;
+
+	while (num1[len1])
+		len1++;
+
+	while (num2[len2])
+		len2++;
+
+	int *result = calloc(len1 + len2, sizeof(int));
+
+	if (result == NULL)
+		error_exit();
+
+	for (int i = len1 - 1; i >= 0; i--)
+	{
+		for (int j = len2 - 1; j >= 0; j--)
+		{
+			int product = (num1[i] - '0') * (num2[j] - '0');
+			int pos1 = i + j;
+			int pos2 = i + j + 1;
+			int sum = product + result[pos2];
+
+			result[pos1] += sum / 10;
+			result[pos2] = sum % 10;
+		}
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
+
+	int i = 0;
+
+	while (result[i] == 0 && i < len1 + len2 - 1)
+		i++;
+
+	for (; i < len1 + len2; i++)
+		printf("%d", result[i]);
+
+	printf("\n");
+
+	free(result);
+
 	return (0);
 }
