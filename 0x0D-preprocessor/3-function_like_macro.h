05f524ed160e8712a494d954a8a515e7255ef833
allendavee <ugiomohlilmars@gmail.com>
2023-03-21 11:50:36 +0000

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef FUNCITION_LIKE_MACRO_H_
 #define FUNCITION_LIKE_MACRO_H_
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-#endif //FUNCITION_LIKE_MACRO_H_
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+#endif /*FUNCITION_LIKE_MACRO_H_*/
