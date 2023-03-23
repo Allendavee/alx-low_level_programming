621be43f0014125cc90a74fa8b3b944801a72963
allendavee <ugiomohlilmars@gmail.com>
2023-03-23 21:39:55 +0000

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,12 +8,12 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
+	va_list valist;
 	unsigned int i = 0, j, c = 0;
 	char *str;
 	const char t_arg[] = "cifs";
 
-	va_start(args, format);
+	va_start(valist, format);
 	while (format && format[i])
 	{
 		j = 0;
@@ -28,24 +28,24 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c", va_arg(args, int)), c = 1;
+			printf("%c", va_arg(valist, int)), c = 1;
 			break;
 		case 'i':
-			printf("%d", va_arg(args, int)), c = 1;
+			printf("%d", va_arg(valist, int)), c = 1;
 			break;
 		case 'f':
-			printf("%f", va_arg(args, double)), c = 1;
+			printf("%f", va_arg(valist, double)), c = 1;
 			break;
 		case 's':
-			str = va_arg(args, char *), c = 1;
-			if (str == NULL)
+			str = va_arg(valist, char *), c = 1;
+			if (!str)
+			{
 				printf("(nil)");
 				break;
-
+			}
 			printf("%s", str);
 			break;
 		} i++;
 	}
-	va_end(args);
-	printf("\n");
+	va_end(valist), printf("\n");
 }
