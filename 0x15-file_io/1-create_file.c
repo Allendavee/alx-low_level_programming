1497bcc5ed395db86904e387a245ed7dd157a6d9
allendavee <ugiomohlilmars@gmail.com>
2023-04-08 17:37:50 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -41,13 +41,13 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
-		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 
 	len_of_text = _strlen(text_content);
 	ret = write(fd, text_content, len_of_text);
 
-	if (ret == -1 (size_t) ret != len_of_text)
+	if (ret == -1)
 	{
 		return (-1);
 	}
