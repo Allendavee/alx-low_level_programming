8442530076d38a8f5b285515b8f29d57085f072d
allendavee <ugiomohlilmars@gmail.com>
2023-04-10 18:01:31 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,25 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
+	int len = 0, base_two;
 
 	if (b == NULL)
 		return (0);
 
-	for (; *b != '\0'; ++b)
-	{
-		if (*b != '0' && *b != '1')
-			return (0);
+	while (b[len] != '\0')
+		len++;
 
-		result <<= 1;
-		if (*b == '1')
-			result += 1;
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			result += base_two;
+		}
 	}
 
 	return (result);
