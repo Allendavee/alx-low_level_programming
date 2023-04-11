1838536fcfa29634a84a7a4d0bd6dd6ed6ccd9a9
allendavee <ugiomohlilmars@gmail.com>
2023-04-11 06:24:47 +0000

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * set_bit - Function to print out the binary representation of
+ * an unsigned long int
+ * @n: Pointer to an unsigned long int
+ * @index: The index to set the value to 1
+ *
+ * Return: 1 if success, -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int revnum;
+	unsigned long int num;
+	unsigned long int count;
+
+	revnum = 0;
+	count = 0;
+	num = 0;
+
+	if (index > 63)
+		return (-1);
+	while (count < 63)
+	{
+		revnum = revnum << 1;
+		if (count == index)
+			revnum += 1;
+		else
+			revnum += n & 1;
+		count++;
+		n = n >> 1;
+	}
+	while (count > 0)
+	{
+		num = num << 1;
+		num += revnum & 1;
+		revnum = revnum >> 1;
+		count--;
+	}
+	*n = num;
+	return (1);
+}
